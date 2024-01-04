15862618e2f2aa59e344750118d8310b620f7fea
Jhtrabskntnu <101297571+Jhtrabskntnu@users.noreply.github.com>
2024-01-04 13:25:01 +0100

    added new positron class

--- src/operators/singlet_excitation_operator_positrons.jl
+++ src/operators/singlet_excitation_operator_positrons.jl
@@ -1,11 +1,11 @@
 export EP, ep
 
 """
-    SingletExcitationOperator Positrons
+    SingletExcitationOperatorPositrons Positrons
 
 The basic EP_pq type operator.
 """
-struct SingletExcitationOperator <: Operator
+struct SingletExcitationOperatorPositronsPositrons <: Operator
     p::Int
     q::Int
 end
@@ -13,28 +13,28 @@ end
 function Base.show(io::IO,
     (
         ep, constraints, translation
-    )::Tuple{SingletExcitationOperator,Constraints,IndexTranslation})
+    )::Tuple{SingletExcitationOperatorPositrons,Constraints,IndexTranslation})
     print(io, "EP_")
     print_mo_index(io, constraints, translation, e.p, e.q)
 end
 
-function exchange_indices(ep::SingletExcitationOperator, mapping)
-    SingletExcitationOperator(
+function exchange_indices(e::SingletExcitationOperatorPositrons, mapping)
+    SingletExcitationOperatorPositrons(
         exchange_index(e.p, mapping),
         exchange_index(e.q, mapping)
     )
 end
 
-function get_all_indices(ep::SingletExcitationOperator)
+function get_all_indices(e::SingletExcitationOperatorPositrons)
     (e.p, e.q)
 end
 
-function Base.:(==)(ap::SingletExcitationOperator, bp::SingletExcitationOperator)
-    (a.p, a.q) == (bp.p, bp.q)
+function Base.:(==)(a::SingletExcitationOperatorPositrons, b::SingletExcitationOperatorPositrons)
+    (a.p, a.q) == (b.p, b.q)
 end
 
-function Base.isless(ap::SingletExcitationOperator, bp::SingletExcitationOperator)
-    (a.p, a.q) < (bp.p, bp.q)
+function Base.isless(a::SingletExcitationOperatorPositrons, b::SingletExcitationOperatorPositrons)
+    (a.p, a.q) < (b.p, b.q)
 end
 
 """
@@ -42,7 +42,7 @@ end
 
 Constructs an expression containing a single excitation operator.
 """
-EP(p, q) = Expression(SingletExcitationOperator(p, q))
+EP(p, q) = Expression(SingletExcitationOperatorPositrons(p, q))
 
 """
     ep(p, q, r, s) = EP(p, q) * EP(r, s) - δ(r, q) * EP(p, s)
@@ -52,19 +52,19 @@ Alias for the two electron singlet excitation operator.
 """
 ep(p, q, r, s) = EP(p, q) * EP(r, s) - δ(r, q) * EP(p, s)
 
-function convert_to_elementary_operators(op::SingletExcitationOperator)
+function convert_to_elementary_operators(o::SingletExcitationOperatorPositrons)
     Expression(
         [(fermiondag(o.p, spin)*fermion(o.q, spin))[1] for spin in (α, β)]
     )
 end
 
-function act_on_ket(op::SingletExcitationOperator)
+function act_on_ket(o::SingletExcitationOperatorPositrons)
     EP = o.p
     q = o.q
     EP(p, q) * virtual(p) * occupied(q) +
     2 * δ(p, q) * occupied(p, q)
 end
 
-function Base.adjoint(op::SingletExcitationOperator)
-    SingletExcitationOperator(o.q, o.p)
+function Base.adjoint(o::SingletExcitationOperatorPositrons)
+    SingletExcitationOperatorPositrons(o.q, o.p)
 end
